bd6989c5ca1fb09f375f488198f59a1766097976
DoggersHusky <robert.ce@webbuildersgroup.com>
2019-12-05 13:49:40 -0400

    removed jquery and started working with the reactdom

--- js-src/components/ImageCropField.jsx
+++ js-src/components/ImageCropField.jsx
@@ -1,5 +1,6 @@
 /* global jQuery */
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 import MoveTool from "./MoveTool.jsx";
@@ -8,6 +9,7 @@ import ZoominTool from "./ZoominTool.jsx";
 import ZoomoutTool from "./ZoomoutTool.jsx";
 import ResetTool from "./ResetTool.jsx";
 import SavecroppedTool from "./SavecroppedTool.jsx";
+import Cropper from "../assets/cropper.min.js";
 
 class ImageCropField extends Component {
   constructor(props) {
@@ -15,26 +17,87 @@ class ImageCropField extends Component {
 
     this.state = {
       loading: false,
+      cropper: null,
     };
 
     this.handleSave = this.handleSave.bind(this);
   }
 
+  /*
+   * when the component has been fully loaded
+   */
+  componentDidMount(e) {
+    let image = ReactDOM.findDOMNode(this.refs.image);
+
+    this.props.cropper = new Cropper(image, {
+      responsive: true,
+      minContainerWidth: 542,
+      minContainerHeight: 500,
+    });
+  }
+
+  /**
+   * handle saving the cropped image
+   */
   handleSave(e) {
-    // const form = e.target.closest("form");
-    // let formUrl = form.getAttribute("action");
-    // let url =
-    //   encodeURI(formUrl) +
-    //   "/field/" +
-    //   e.target.closest(".imagecrop-field").getAttribute("name") +
-    //   "/cropImage";
-    // //find the cropper
-    // let target = e.target
-    //   .closest(".imagecrop-field")
-    //   .querySelector(".imagecrop-field-selection");
+    const form = e.target.closest("form");
+    let formUrl = form.getAttribute("action");
+    let self = this;
+    let url =
+      encodeURI(formUrl) +
+      "/field/" +
+      e.target.closest(".imagecrop-field").getAttribute("name") +
+      "/cropImage";
+
+    //find the cropper
+    let cropper = this.props.cropper;
+
+    //the cropped image
+    let data = {
+      image: cropper.getCroppedCanvas().toDataURL(),
+    };
+
+    //add loading
     this.setState({
       loading: true,
     });
+
+    this.postAjax(url, data, function(data) {
+      console.log(data);
+
+      //remove loading
+      self.setState({
+        loading: false,
+      });
+    });
+  }
+
+  /**
+   * allows us to make simple post request
+   */
+  postAjax(url, data, success) {
+    var params =
+      typeof data == "string"
+        ? data
+        : Object.keys(data)
+            .map(function(k) {
+              return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+            })
+            .join("&");
+
+    var xhr = window.XMLHttpRequest
+      ? new XMLHttpRequest()
+      : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open("POST", url);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState > 3 && xhr.status == 200) {
+        success(xhr.responseText);
+      }
+    };
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+    return xhr;
   }
 
   /**
@@ -44,7 +107,7 @@ class ImageCropField extends Component {
     let loadingSpinner = null;
     if (this.state.loading) {
       // const Loading = this.props.LoadingComponent;
-      loadingSpinner = loading;
+      loadingSpinner = "loading";
     }
 
     return (
@@ -62,6 +125,7 @@ class ImageCropField extends Component {
           <img
             class="imagecrop-field-selection"
             src={this.props.data.image}
+            ref="image"
           ></img>
         </div>
       </div>
@@ -70,18 +134,3 @@ class ImageCropField extends Component {
 }
 
 export default ImageCropField;
-
-// jquery to handle the image field
-jQuery.entwine("ImageCropMain", function($) {
-  //handle adding the field to the image
-  $(".imagecrop-field-selection").entwine({
-    onmatch: function() {
-      //add the cropper to the image
-      $(this).cropper({
-        responsive: true,
-        minContainerWidth: 542,
-        minContainerHeight: 500,
-      });
-    },
-  });
-});
